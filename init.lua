820d15da3556f0d56375b5ea3243497ecba38666
octacian <enduffy2014@outlook.com>
2017-07-24 17:40:32 -0700

    Fix crash `on_dignode` without player parameter

--- init.lua
+++ init.lua
@@ -13,22 +13,24 @@ local modpath   = minetest.get_modpath("gamemode")
 
 -- [register] On join set gamemode
 minetest.register_on_joinplayer(function(player)
-  gamemode.set(player, gamemode.get(player))
+	gamemode.set(player, gamemode.get(player))
 end)
 
 -- [register] On place node
 minetest.register_on_placenode(function(pos, newnode, player, oldnode, itemstack)
-  local mode = gamemode.def(gamemode.get(player))
-	local name = player:get_player_name()
+	if player then
+		local mode = gamemode.def(gamemode.get(player))
+		local name = player:get_player_name()
 
-	if not gamemode.can_interact(player) then
-		minetest.remove_node(pos)
-		minetest.chat_send_player(name, minetest.colorize("red", "WARNING")..
-				" You cannot place nodes in "..mode.name.." mode")
-		return true
+		if not gamemode.can_interact(player) then
+			minetest.remove_node(pos)
+			minetest.chat_send_player(name, minetest.colorize("red", "WARNING")..
+					" You cannot place nodes in "..mode.name.." mode")
+			return true
+		end
+
+		return mode.stack_unlimited
 	end
-
-  return mode.stack_unlimited
 end)
 
 -- [register] On dig node
@@ -45,13 +47,13 @@ end)
 
 -- [register] On hp change
 minetest.register_on_player_hpchange(function(player, hp_change)
-  local def = gamemode.def(gamemode.get(player))
+	local def = gamemode.def(gamemode.get(player))
 	player:set_breath(11)
-  if def.damage == false then
-    return 0
-  else
-    return hp_change
-  end
+	if def.damage == false then
+		return 0
+	else
+		return hp_change
+	end
 end, true)
 
 -- [register] On breath change
@@ -103,14 +105,14 @@ end)
 
 local rotate_node = minetest.rotate_node
 function minetest.rotate_node(itemstack, placer, pointed_thing)
-  itemstack = rotate_node(itemstack, placer, pointed_thing)
+	itemstack = rotate_node(itemstack, placer, pointed_thing)
 
-  local mode = gamemode.def(gamemode.get(placer))
-  if not mode.stack_unlimited then
-    itemstack:take_item(1)
-  end
+	local mode = gamemode.def(gamemode.get(placer))
+	if not mode.stack_unlimited then
+		itemstack:take_item(1)
+	end
 
-  return itemstack
+	return itemstack
 end
 
 ---
@@ -119,19 +121,19 @@ end
 
 -- [function] Register gamemode
 function gamemode.register(name, def)
-  -- Register hand
-  if def.hand then
-    minetest.register_item("gamemode:"..name, {
-      type = "none",
-      wield_image = def.hand.wield_image or "wieldhand.png",
-      wield_scale = {x = 1, y = 1, z = 2.5},
-      range = def.hand.range,
-      tool_capabilities = def.hand,
+	-- Register hand
+	if def.hand then
+		minetest.register_item("gamemode:"..name, {
+			type = "none",
+			wield_image = def.hand.wield_image or "wieldhand.png",
+			wield_scale = {x = 1, y = 1, z = 2.5},
+			range = def.hand.range,
+			tool_capabilities = def.hand,
 			on_use = def.hand.on_use,
-    })
+		})
 
 		def.hand = "gamemode:"..name
-  end
+	end
 
 	-- Save aliases
 	if def.aliases then
@@ -146,64 +148,64 @@ end
 
 -- [function] Get gamemode definition
 function gamemode.def(name)
-  if gamemodes[name] then
-    return gamemodes[name]
+	if gamemodes[name] then
+		return gamemodes[name]
 	else
 		if aliases[name] then
 			return gamemodes[aliases[name]]
 		end
-  end
+	end
 end
 
 -- [function] Set player gamemode
 function gamemode.set(player, gm_name)
 	local gm = gamemode.def(gm_name)
-  if gm then
-    local name = player:get_player_name()
+	if gm then
+		local name = player:get_player_name()
 		local privs = minetest.get_player_privs(name)
 
-    local old_gm = gamemode.def(gamemode.get(player))
-    -- Revert HUD flags
-    if old_gm.hud_flags then
-      local flags = table.copy(old_gm.hud_flags)
-      for _, f in pairs(flags) do
-        flags[_] = not f
-      end
-      player:hud_set_flags(flags)
-    end
+		local old_gm = gamemode.def(gamemode.get(player))
+		-- Revert HUD flags
+		if old_gm.hud_flags then
+			local flags = table.copy(old_gm.hud_flags)
+			for _, f in pairs(flags) do
+				flags[_] = not f
+			end
+			player:hud_set_flags(flags)
+		end
 		-- Revert privileges
-    if old_gm.privileges then
-      for _, i in pairs(old_gm.privileges) do
-        privs[_] = not old_gm.privileges[_] or nil
-      end
-    end
-    -- Check for on disable
-    if old_gm.on_disable then
-      old_gm.on_disable(player)
-    end
+		if old_gm.privileges then
+			for _, i in pairs(old_gm.privileges) do
+				privs[_] = not old_gm.privileges[_] or nil
+			end
+		end
+		-- Check for on disable
+		if old_gm.on_disable then
+			old_gm.on_disable(player)
+		end
 
-    -- Update cache
-    player:set_attribute("gamemode", gm_name)
-    -- Update HUD flags
-    if gm.hud_flags then
-      player:hud_set_flags(gm.hud_flags)
-    end
-    -- Check for on enable
-    if gm.on_enable then
-      gm.on_enable(player)
-    end
-    -- Update privileges
-    if gm.privileges then
-      for _, i in pairs(gm.privileges) do
-        privs[_] = gm.privileges[_] or nil
-      end
-    end
-    -- Update hand
-    if gm.hand then
-      player:get_inventory():set_stack("hand", 1, gm.hand)
+		-- Update cache
+		player:set_attribute("gamemode", gm_name)
+		-- Update HUD flags
+		if gm.hud_flags then
+			player:hud_set_flags(gm.hud_flags)
+		end
+		-- Check for on enable
+		if gm.on_enable then
+			gm.on_enable(player)
+		end
+		-- Update privileges
+		if gm.privileges then
+			for _, i in pairs(gm.privileges) do
+				privs[_] = gm.privileges[_] or nil
+			end
+		end
+		-- Update hand
+		if gm.hand then
+			player:get_inventory():set_stack("hand", 1, gm.hand)
 		else -- else, Reset hand
 			player:get_inventory():set_stack("hand", 1, "")
-    end
+		end
 		-- Show/hide healthbar and breathbar
 		player:hud_set_flags({
 			healthbar = gm.damage ~= false,
@@ -219,27 +221,27 @@ function gamemode.set(player, gm_name)
 			sfinv.set_page(player, sfinv.pages_unordered[1].name)
 		end
 
-    return true
-  end
+		return true
+	end
 end
 
 -- [function] Get player gamemode
 function gamemode.get(player)
-  if type(player) == "string" then
-    player = minetest.get_player_by_name(player)
-  end
+	if type(player) == "string" then
+		player = minetest.get_player_by_name(player)
+	end
 
-  local gm = player:get_attribute("gamemode")
-  if not gm or gm == "" then
-    if creative then gm = "creative"
-    else gm = "survival" end
-  end
+	local gm = player:get_attribute("gamemode")
+	if not gm or gm == "" then
+		if creative then gm = "creative"
+		else gm = "survival" end
+	end
 
 	if not gamemode.def(gm) then
 		return "survival"
 	end
 
-  return gm
+	return gm
 end
 
 -- [function] Can interact
@@ -261,11 +263,11 @@ minetest.register_privilege("gamemode", "Ability to use /gamemode")
 
 -- [command] Gamemode
 minetest.register_chatcommand("gamemode", {
-  description = "Change gamemode (providing no gamemode parameter returns "..
+	description = "Change gamemode (providing no gamemode parameter returns "..
 			"the player's gamemode)",
-  params = "(<player name>) [<gamemode> | list]",
-  privs = {gamemode=true},
-  func = function(name, param)
+	params = "(<player name>) [<gamemode> | list]",
+	privs = {gamemode=true},
+	func = function(name, param)
 		local params = param:split(" ")
 		local player = minetest.get_player_by_name(name)
 		local newgm
@@ -319,12 +321,12 @@ minetest.register_chatcommand("gamemode", {
 
 		-- Set gamemode
 		if newgm and gamemode.set(player, newgm) then
-	    return true, "Set "..player:get_player_name().."'s gamemode to "..
+			return true, "Set "..player:get_player_name().."'s gamemode to "..
 					gamemode.def(newgm).caption:lower()
 		else -- else, Return invalid gamemode
 			return false, "Invalid gamemode "..dump(newgm)
 		end
-  end,
+	end,
 })
 
 ---
