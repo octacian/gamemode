8da94e20a2edac5cb092547aeb6713f8267c0f42
Lone_Wolf <lonewolf04361@gmail.com>
2018-01-03 23:55:46 -0800

    Fix creative mode removing armor tab

--- init.lua
+++ init.lua
@@ -97,6 +97,19 @@ minetest.after(0, function()
 			end
 		end
 	end
+		sfinv.register_page("3d_armor:armor2", {
+		title = ("Armor"),
+		get = function(self, player, context)
+			local name = player:get_player_name()
+			local formspec = armor:get_armor_formspec(name, true)
+			return sfinv.make_formspec(player, context, formspec, false)
+		end
+	})
+	armor:register_on_update(function(player)
+		if sfinv.enabled then
+			sfinv.set_player_inventory_formspec(player)
+		end
+	end)
 end)
 
 ---
